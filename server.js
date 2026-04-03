0c2453512cefd1b9496aebde99b170e182a541de
LazuPastqs <100015952+LazuPastqs@users.noreply.github.com>
2026-04-03 22:31:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-require('dotenv').config();
-
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const SECRET_KEY = process.env.SECRET_KEY;
 
 if (!SECRET_KEY) {
